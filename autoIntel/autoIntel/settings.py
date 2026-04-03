4d1a88867e18cf01bc90242d1135a46821179c2a
DIVYANSH1610 <142164770+DIVYANSH1610@users.noreply.github.com>
2026-04-04 03:45:10 +0530

    Update settings.py

--- autoIntel/autoIntel/settings.py
+++ autoIntel/autoIntel/settings.py
@@ -177,13 +177,13 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-frontend.vercel.app",
+    "https://moto-e.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://your-frontend.vercel.app",
+    "https://moto-e.vercel.app",
 ]
 
 
