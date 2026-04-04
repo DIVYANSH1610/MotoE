b70b405e0952a290c1e131910b00fd9e93b9488b
DIVYANSH1610 <142164770+DIVYANSH1610@users.noreply.github.com>
2026-04-04 22:08:02 +0530

    Update auth_views.py

--- autoIntel/cars/auth_views.py
+++ autoIntel/cars/auth_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -12,7 +13,10 @@ User = get_user_model()
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def csrf(request):
-    return Response({"message": "CSRF cookie set"})
+    return Response({
+        "message": "CSRF cookie set",
+        "csrfToken": get_token(request)
+    })
 
 
 @api_view(["POST"])
